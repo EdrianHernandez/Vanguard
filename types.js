85adfc9d98a590190dbf5644955de15e78af2142
Edrian Hernandez <117151322+EdrianHernandez@users.noreply.github.com>
2026-01-08 16:22:06 +0800

    Update and rename types.ts to types.js

---
+++ types.js
@@ -0,0 +1,14 @@
+export const LeadStatus = {
+  NEW: 'New',
+  CONTACTED: 'Contacted',
+  QUALIFIED: 'Qualified',
+  PROPOSAL: 'Proposal',
+  NEGOTIATION: 'Negotiation',
+  CLOSED: 'Closed'
+};
+
+export const TaskStatus = {
+  TODO: 'To Do',
+  IN_PROGRESS: 'In Progress',
+  DONE: 'Done'
+};
